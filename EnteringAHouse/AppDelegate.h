7b1a62c8fdbfe5c4e160f1236c5b38fdb7708054
pietbinon <pierrebinon@icloud.com>
2017-03-15 17:05:34 -0400

    Initial Commit

---
+++ EnteringAHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnteringAHouse
+//
+//  Created by Pierre Binon on 2017-03-15.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by Pierre Binon on 2017-03-15.
+//  Copyright © 2017 Pierre Binon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
